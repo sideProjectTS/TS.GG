c6b3f7eac329064bb2edcbc449d4d4e0c485162e
Seungmin <rnsdls1997@gmail.com>
2025-11-09 21:46:27 +0900

    feat: cors 설정 추가

--- backend/src/main/java/gg/ts/app/config/WebConfig.java
+++ backend/src/main/java/gg/ts/app/config/WebConfig.java
@@ -2,10 +2,20 @@ package gg.ts.app.config;
 
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 @EnableWebMvc
 @ComponentScan(basePackages = "ts.gg.app")   // TODO: 디렉터리 구조 회의
-public class WebConfig {
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://locallhost:5173")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowCredentials(true);
+    }
 }
